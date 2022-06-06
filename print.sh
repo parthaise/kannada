4ded247a81c2d6511f85cd956238b8912a31f8c3
testuser <parthaise21@gmail.com>
2022-06-06 09:59:53 +0530

    added three files

---
+++ print.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+country="INDIA"
+year=2022
+state="karnataka"
+district="Hassan"
+
+echo "countryname:$country"
+echo "year:$year"
+echo "state:$state"
+echo "district:$district"
---
+++ ss.sh
@@ -0,0 +1,6 @@
+#!/bin/bash/
+num1=50
+num2=30
+sum='expr $num1+ $num2'
+echo "Sum is :$sum"
+
